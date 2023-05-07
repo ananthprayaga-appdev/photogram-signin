1b941ece34e5e0c87eb7f641f250a33dadd27711
Ananth Prayaga <ananthp@uchicago.edu>
2023-05-07 14:51:19 +0000

    Finished Project

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,11 +1,11 @@
 class PhotosController < ApplicationController
   def index
     @photos = Photo.all
-    render({ :template => "photos/all_photos.html.erb"})
+    render({ :template => "photos/all_photos.html.erb" })
   end
 
   def create
-    user_id = params.fetch("input_owner_id")
+    user_id = session.fetch(:user_id)
     image = params.fetch("input_image")
     caption = params.fetch("input_caption")
     photo = Photo.new
@@ -17,9 +17,10 @@ class PhotosController < ApplicationController
   end
 
   def show
+    @session_user_id = session.fetch(:user_id)
     p_id = params.fetch("the_photo_id")
-    @photo = Photo.where({:id => p_id }).first
-    render({:template => "photos/details.html.erb"})
+    @photo = Photo.where({ :id => p_id }).first
+    render({ :template => "photos/details.html.erb" })
   end
 
   def destroy
