b48606bbdbbf30c4ecf7dbbbe8ba7c7000b184fe
Ananth Prayaga <ananthp@uchicago.edu>
2023-05-07 14:24:16 +0000

    User Sign Up, Sign In & Sign Out completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,37 @@ class UsersController < ApplicationController
     render({ :template => "users/index.html" })
   end
 
+  def sign_up
+    render({ :template => "users/signup.html.erb" })
+  end
+
+  def sign_in
+    render({ :template => "users/signin.html.erb" })
+  end
+
+  def authenticate
+    un = params.fetch("input_username")
+    pw = params.fetch("input_password")
+
+    user = User.where({ :username => un }).at(0)
+
+    if user == nil
+      redirect_to("/user_sign_in", { :alert => "No one around here with that name" })
+    else
+      if user.authenticate(pw)
+        session.store(:user_id, user.id)
+        redirect_to("/", { :notice => "Welcome back, " + user.username + "!" })
+      else
+        redirect_to("/user_sign_in", { :alert => "Nice try, sucker!" })
+      end
+    end
+  end
+
+  def sign_out
+    reset_session
+    redirect_to("/", { :notice => "See you later" })
+  end
+
   def show
     the_username = params.fetch("the_username")
     @user = User.where({ :username => the_username }).at(0)
@@ -16,21 +47,27 @@ class UsersController < ApplicationController
     user = User.new
 
     user.username = params.fetch("input_username")
+    user.password = params.fetch("input_password")
+    user.password_confirmation = params.fetch("input_password_confirmation")
 
-    user.save
+    save_status = user.save
 
-    redirect_to("/users/#{user.username}")
+    if save_status == true
+      session.store(:user_id, user.id)
+      redirect_to("/users/#{user.username}", { :notice => "Welcome, " + user.username + " !" })
+    else
+      redirect_to("/user_sign_up", { :alert => user.errors.full_messages.to_sentence })
+    end
   end
 
   def update
     the_id = params.fetch("the_user_id")
     user = User.where({ :id => the_id }).at(0)
 
-
     user.username = params.fetch("input_username")
 
     user.save
-    
+
     redirect_to("/users/#{user.username}")
   end
 
@@ -42,5 +79,4 @@ class UsersController < ApplicationController
 
     redirect_to("/users")
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,26 @@
 Rails.application.routes.draw do
-
   get("/", { :controller => "users", :action => "index" })
 
   # User routes
 
+  # SIGN UP
+  get("/user_sign_up", { :controller => "users", :action => "sign_up" })
+  get("/user_sign_in", { :controller => "users", :action => "sign_in" })
+  get("/user_sign_out", { :controller => "users", :action => "sign_out" })
+  post("/verify_credentials", { :controller => "users", :action => "authenticate" })
+
   # CREATE
-  get("/insert_user_record", {:controller => "users", :action => "create" })
+  get("/insert_user_record", { :controller => "users", :action => "create" })
 
   # READ
-  get("/users", {:controller => "users", :action => "index"})
-  get("/users/:the_username", {:controller => "users", :action => "show"})
+  get("/users", { :controller => "users", :action => "index" })
+  get("/users/:the_username", { :controller => "users", :action => "show" })
 
   # UPDATE
-  get("/update_user/:the_user_id", {:controller => "users", :action => "update" })
+  get("/update_user/:the_user_id", { :controller => "users", :action => "update" })
 
   # DELETE
-  get("/delete_user/:the_user_id", {:controller => "users", :action => "destroy"})
+  get("/delete_user/:the_user_id", { :controller => "users", :action => "destroy" })
 
   # Photo routes
 
@@ -23,15 +28,15 @@ Rails.application.routes.draw do
   get("/insert_photo_record", { :controller => "photos", :action => "create" })
 
   # READ
-  get("/photos", { :controller => "photos", :action => "index"})
+  get("/photos", { :controller => "photos", :action => "index" })
 
-  get("/photos/:the_photo_id", { :controller => "photos", :action => "show"})
+  get("/photos/:the_photo_id", { :controller => "photos", :action => "show" })
 
   # UPDATE
   get("/update_photo/:the_photo_id", { :controller => "photos", :action => "update" })
 
   # DELETE
-  get("/delete_photo/:the_photo_id", { :controller => "photos", :action => "destroy"})
+  get("/delete_photo/:the_photo_id", { :controller => "photos", :action => "destroy" })
 
   # Comment routes
 
@@ -40,6 +45,5 @@ Rails.application.routes.draw do
 
   # DELETE
 
-  get("/delete_comment/:the_comment_id", { :controller => "comments", :action => "destroy"})
-
+  get("/delete_comment/:the_comment_id", { :controller => "comments", :action => "destroy" })
 end
